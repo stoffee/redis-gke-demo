29a8a483bcaa9166acd87e42f7f68b9ae0d9edb2
stoffee <chris@roadkill.org>
2019-04-27 16:39:31 -0700

    added main.tf vars.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "gcp_project_id" {
+  description = "The name of the GCP Project where all resources will be launched."
+}
+
+variable "gcp_credentials" {
+  description = "The name of the GCP Project where all resources will be launched."
+}
+
+variable "gcp_region" {
+  description = "The region in which all GCP resources will be launched."
+}
+
+variable "location_id" {
+  description = "The location in which all GCP resources will be launched."
+}
+
+variable "alternative_location_id" {
+  description = "The alternative location in which all GCP resources will be launched."
+}
+
+variable "namespace" {
+  description = "namespace"
+}
