664b4fd1ba9a0482393107f90de2f53f6bb6bb80
stoffee <chris@roadkill.org>
2019-04-27 17:03:17 -0700

    updated auto-network

--- main.tf
+++ main.tf
@@ -10,27 +10,27 @@ terraform {
 
 
 resource "google_redis_instance" "cache" {
-  name           = "ha-memory-cache"
+  name           = "${var.namespace}-cache"
   tier           = "STANDARD_HA"
   memory_size_gb = 1
 
-#  location_id             = "us-central1-a"
   location_id             = "${var.gcp_zone}"
   alternative_location_id = "${var.alternative_gcp_zone}"
-  project       = "${var.gcp_project_id}"
 
   authorized_network = "${google_compute_network.auto-network.self_link}"
 
   redis_version     = "REDIS_3_2"
   display_name      = "${var.namespace}"
-  reserved_ip_range = "192.168.0.0/29"
+#  reserved_ip_range = "192.168.0.0/29"
+  auto_create_subnetwork = "True"
 
   labels = {
-    Owner    = "cdunlap"
-    TTL = "24hrs"
+    Owner   = "cdunlap"
+    TTL     = "24hrs"
   }
 }
 
 resource "google_compute_network" "auto-network" {
   name = "authorized-network"
+  project       = "${var.gcp_project_id}"
 }
