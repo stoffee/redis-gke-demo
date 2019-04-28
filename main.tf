85f2a2f4fcf92e6bf95170fec5857efa88a3dfb9
stoffee <chris@roadkill.org>
2019-04-27 17:58:04 -0700

    merging vars

--- main.tf
+++ main.tf
@@ -12,5 +12,11 @@ module "redis" {
  # to a specific version of the modules, such as the following example:
  # source = "modules/vault-cluster"
  source = "git::https://github.com/stoffee/redis-gke-demo.git//modules/redis"
+  region      = "${var.gcp_region}"
+  credentials = "${var.gcp_credentials}"
+  gcp_project_id = "${var.gcp_project_id}"
+  gcp_zone = "${var.gcp_zone}"
+  alternative_gcp_zone = "${var.alternative_gcp_zone}"
+  namespace = "${var.namespace}"
 
 }
