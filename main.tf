d13084d1807e3365ac63fab81d6def3fc0471097
stoffee <chris@roadkill.org>
2019-04-27 17:50:50 -0700

    restructured

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  region      = "${var.gcp_region}"
+  credentials = "${var.gcp_credentials}"
+}
+
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+module "redis" {
+# When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
+ # to a specific version of the modules, such as the following example:
+ # source = "modules/vault-cluster"
+ source = "git::https://github.com/stoffee/redis-gke-demo.git//modules/redis"
+
+}
