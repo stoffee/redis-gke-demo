f7223d370c35062399a16c17ff057c97bc192708
stoffee <chris@roadkill.org>
2019-04-27 18:10:25 -0700

    added gke module

--- main.tf
+++ main.tf
@@ -20,3 +20,17 @@ module "redis" {
   namespace = "${var.namespace}"
 
 }
+
+module "gke" {
+# When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
+ # to a specific version of the modules, such as the following example:
+ # source = "modules/vault-cluster"
+ source = "git::https://github.com/stoffee/redis-gke-demo.git//modules/gke"
+  gcp_region      = "${var.gcp_region}"
+  gcp_credentials = "${var.gcp_credentials}"
+  gcp_project_id = "${var.gcp_project_id}"
+  gcp_zone = "${var.gcp_zone}"
+  alternative_gcp_zone = "${var.alternative_gcp_zone}"
+  namespace = "${var.namespace}"
+
+}
